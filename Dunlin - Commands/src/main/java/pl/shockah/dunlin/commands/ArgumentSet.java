82b17d1a2d19fab5290beedd83a121e908168fc2
Michael <lolshockah@gmail.com>
2017-02-16 10:45:35 +0100

    Restructuring

---
+++ Dunlin - Commands/src/main/java/pl/shockah/dunlin/commands/ArgumentSet.java
@@ -0,0 +1,29 @@
+package pl.shockah.dunlin.commands;
+
+import java.lang.reflect.Field;
+
+public abstract class ArgumentSet {
+	void parse(String textInput) {
+	}
+	
+	public void onUnknownArgument(String argumentName, String rawValue) {
+		throw new IllegalArgumentException(String.format("Unknown argument %s.", argumentName));
+	}
+	
+	public boolean isValueValid(Field field, Object value) {
+		return true;
+	}
+	
+	public void finalValidation() {
+	}
+	
+	public static @interface Argument {
+		String value() default "";
+		boolean isDefault() default false;
+		ArgumentType type() default ArgumentType.Auto;
+	}
+	
+	public static enum ArgumentType {
+		@Deprecated Auto, Integer, Decimal, Range, String, Bool, Enum;
+	}
+}
--- Dunlin/src/main/java/pl/shockah/dunlin/db/SQLExceptionWrappedAction1.java
+++ Dunlin/src/main/java/pl/shockah/dunlin/db/SQLExceptionWrappedAction1.java
@@ -4,5 +4,5 @@ import java.sql.SQLException;
 
 @FunctionalInterface
 public interface SQLExceptionWrappedAction1<A> {
-	public void call(A t1) throws SQLException;
+	void call(A t1) throws SQLException;
 }
