25dbfacf90c4f76b6bd2b238c49ca9f9669367ac
Michael <lolshockah@gmail.com>
2017-02-22 10:39:11 +0100

    [Owner] .user is now generic (works for non-members too)

--- Dunlin - Owner/src/main/java/pl/shockah/dunlin/owner/UserCommand.java
+++ Dunlin - Owner/src/main/java/pl/shockah/dunlin/owner/UserCommand.java
@@ -1,10 +1,12 @@
 package pl.shockah.dunlin.owner;
 
 import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.MessageBuilder;
 import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.Role;
+import net.dv8tion.jda.core.entities.User;
 import org.apache.commons.lang3.StringUtils;
 import pl.shockah.dunlin.commands.NamedCommand;
 import pl.shockah.dunlin.commands.result.CommandResult;
@@ -15,12 +17,14 @@ import pl.shockah.dunlin.util.TimeDuration;
 
 import java.sql.Date;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
-public class UserCommand extends NamedCommand<Member, Member> {
-	public static final Pattern USER_MENTION = Pattern.compile("\\<\\@(\\d+)\\>");
+public class UserCommand extends NamedCommand<User, User> {
+	public static final Pattern USER_MENTION = Pattern.compile("\\<\\@\\!?(\\d+)\\>");
 	public static final Pattern USER_DISCRIMINATOR = Pattern.compile("(.*?)\\#(\\d{4})");
 
 	protected final OwnerPlugin ownerPlugin;
@@ -31,66 +35,81 @@ public class UserCommand extends NamedCommand<Member, Member> {
 	}
 
 	@Override
-	public CommandResult<Member> parseInput(Message message, String textInput) {
-		Member member = null;
+	public CommandResult<User> parseInput(Message message, String textInput) {
+		User user = null;
 
 		if (StringUtils.isBlank(textInput)) {
-			member = message.getGuild().getMember(message.getAuthor());
+			user = message.getAuthor();
 		} else {
 			Matcher matcher;
 
-			if (member == null) {
+			if (user == null) {
 				matcher = USER_MENTION.matcher(textInput);
 				if (matcher.find()) {
 					String userId = matcher.group(1);
-					member = message.getGuild().getMemberById(userId);
+					user = ownerPlugin.manager.app.getShardManager().getUserById(userId);
 				}
 			}
 
-			if (member == null) {
+			if (user == null) {
 				matcher = USER_DISCRIMINATOR.matcher(textInput);
 				if (matcher.find()) {
 					String name = matcher.group(1);
 					String discriminator = matcher.group(2);
-					member = message.getGuild().getMembers().stream()
-							.filter(m -> m.getUser().getName().equalsIgnoreCase(name) && m.getUser().getDiscriminator().equals(discriminator))
-							.findAny().get();
+					user = Arrays.stream(ownerPlugin.manager.app.getShardManager().shards)
+							.map(JDA::getUsers)
+							.flatMap(List::stream)
+							.filter(u -> u.getName().equalsIgnoreCase(name) && u.getDiscriminator().equals(discriminator))
+							.findAny().orElse(null);
 				}
 			}
 
-			if (member == null) {
-				member = message.getGuild().getMembers().stream()
+			if (user == null) {
+				user = message.getGuild().getMembers().stream()
 						.filter(m -> m.getEffectiveName().equalsIgnoreCase(textInput))
-						.findAny().get();
+						.map(Member::getUser)
+						.findAny().orElse(null);
+			}
+
+			if (user == null) {
+				try {
+					user = ownerPlugin.manager.app.getShardManager().getUserById((String.valueOf(Long.parseLong(textInput))));
+				} catch (Exception e) {
+				}
 			}
 		}
 
-		if (member == null)
+		if (user == null)
 			return new ErrorCommandResultImpl<>(this, new MessageBuilder().setEmbed(new EmbedBuilder()
 					.setColor(ErrorCommandResultImpl.EMBED_COLOR)
 					.setDescription(String.format("Cannot find user `%s`.", textInput))
 					.build()).build());
 
-		return new ParseCommandResultImpl<>(this, member);
+		return new ParseCommandResultImpl<>(this, user);
 	}
 
 	@Override
-	public CommandResult<Member> execute(Message message, Member input) {
+	public CommandResult<User> execute(Message message, User input) {
 		return new ValueCommandResultImpl<>(this, input);
 	}
 
 	@Override
-	public Message formatOutput(Message message, Member input, Member output) {
-		return new MessageBuilder().setEmbed(new EmbedBuilder()
-				.setThumbnail(output.getUser().getEffectiveAvatarUrl())
-				.addField("Nickname", output.getEffectiveName(), true)
-				.addField("User", String.format("%s#%s", output.getUser().getName(), output.getUser().getDiscriminator()), true)
-				.addField("ID", output.getUser().getId(), true)
-				.addField("Joined", String.format("%s (%s ago)",
-						output.getJoinDate().format(DateTimeFormatter.ISO_OFFSET_DATE_TIME),
-						TimeDuration.format(Date.from(output.getJoinDate().toInstant()))),
-						true)
-				.addField("Roles", output.getRoles().stream().map(Role::getName).collect(Collectors.joining(", ")), false)
-				.build()).build();
+	public Message formatOutput(Message message, User input, User output) {
+		Member member = message.getGuild().getMember(output);
+
+		EmbedBuilder embedBuilder = new EmbedBuilder();
+		embedBuilder.setThumbnail(output.getEffectiveAvatarUrl());
+
+		embedBuilder.addField("User", String.format("%s#%s", output.getName(), output.getDiscriminator()), true);
+		if (member != null && member.getNickname() != null)
+			embedBuilder.addField("Nickname", member.getNickname(), true);
+		embedBuilder.addField("ID", output.getId(), true);
+		if (member != null)
+			embedBuilder.addField("Joined", TimeDuration.format(Date.from(member.getJoinDate().toInstant())), true);
+
+		if (member != null && !member.getRoles().isEmpty())
+			embedBuilder.addField("Roles", member.getRoles().stream().map(Role::getName).collect(Collectors.joining(", ")), false);
+
+		return new MessageBuilder().setEmbed(embedBuilder.build()).build();
 	}
 }
