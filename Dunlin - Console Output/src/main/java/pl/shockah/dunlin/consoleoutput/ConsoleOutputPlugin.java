c8684d833c6bd514848b2b428360f7cdd826457e
Michael <lolshockah@gmail.com>
2017-02-28 12:09:47 +0100

    [Console Output] Printing embeds to console

--- Dunlin - Console Output/src/main/java/pl/shockah/dunlin/consoleoutput/ConsoleOutputPlugin.java
+++ Dunlin - Console Output/src/main/java/pl/shockah/dunlin/consoleoutput/ConsoleOutputPlugin.java
@@ -1,9 +1,13 @@
 package pl.shockah.dunlin.consoleoutput;
 
+import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.Message.Attachment;
-import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
-import net.dv8tion.jda.core.events.message.MessageUpdateEvent;
+import net.dv8tion.jda.core.entities.MessageEmbed;
+import net.dv8tion.jda.core.entities.TextChannel;
+import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.core.events.message.guild.GuildMessageUpdateEvent;
+import org.apache.commons.lang3.StringUtils;
 import pl.shockah.dunlin.plugin.ListenerPlugin;
 import pl.shockah.dunlin.plugin.PluginManager;
 
@@ -13,38 +17,49 @@ public class ConsoleOutputPlugin extends ListenerPlugin {
 	}
 	
 	@Override
-	protected void onMessageReceived(MessageReceivedEvent event) {
-		Message message = event.getMessage();
-		
-		String rawContent = message.getRawContent();
-		if (!rawContent.isEmpty()) {
-			for (String line : rawContent.split("\\n")) {
-				System.out.println(String.format("%s->#%s | <%s#%s> %s",
-					event.getGuild().getName(), event.getChannel().getName(), event.getAuthor().getName(), event.getAuthor().getDiscriminator(), line));
-			}
-		}
-		
-		for (Attachment attachment : message.getAttachments()) {
-			System.out.println(String.format("%s->#%s | <%s#%s> Attachment: ",
-				event.getGuild().getName(), event.getChannel().getName(), event.getAuthor().getName(), event.getAuthor().getDiscriminator(), attachment.getUrl()));
-		}
+	protected void onGuildMessageReceived(GuildMessageReceivedEvent event) {
+		logMessage(event.getMessage(), false);
 	}
 	
 	@Override
-	protected void onMessageUpdate(MessageUpdateEvent event) {
-		Message message = event.getMessage();
-		
+	protected void onGuildMessageUpdate(GuildMessageUpdateEvent event) {
+		logMessage(event.getMessage(), true);
+	}
+
+	protected void logMessage(Message message, boolean edit) {
+		Guild guild = message.getGuild();
+		TextChannel channel = message.getTextChannel();
+
 		String rawContent = message.getRawContent();
-		if (!rawContent.isEmpty()) {
+		if (!StringUtils.isEmpty(rawContent)) {
 			for (String line : rawContent.split("\\n")) {
-				System.out.println(String.format("%s->#%s | * <%s#%s> %s",
-					event.getGuild().getName(), event.getChannel().getName(), event.getAuthor().getName(), event.getAuthor().getDiscriminator(), line));
+				System.out.println(String.format("%s->#%s |%s<%s#%s> %s",
+						guild.getName(), channel.getName(), edit ? " * " : " ", message.getAuthor().getName(), message.getAuthor().getDiscriminator(), line));
 			}
 		}
-		
+
 		for (Attachment attachment : message.getAttachments()) {
-			System.out.println(String.format("%s->#%s | * <%s#%s> Attachment: %s",
-				event.getGuild().getName(), event.getChannel().getName(), event.getAuthor().getName(), event.getAuthor().getDiscriminator(), attachment.getUrl()));
+			System.out.println(String.format("%s->#%s |%s<%s#%s> Attachment: %s",
+					guild.getName(), channel.getName(), edit ? " * " : " ", message.getAuthor().getName(), message.getAuthor().getDiscriminator(), attachment.getUrl()));
+		}
+
+		for (MessageEmbed embed : message.getEmbeds()) {
+			System.out.println(String.format("%s->#%s |%s<%s#%s> Embed:",
+					guild.getName(), channel.getName(), edit ? " * " : " ", message.getAuthor().getName(), message.getAuthor().getDiscriminator()));
+
+			if (!StringUtils.isEmpty(embed.getTitle()))
+				System.out.println(String.format("\tTitle: %s", embed.getTitle()));
+			if (embed.getAuthor() != null)
+				System.out.println(String.format("\tAuthor: %s", embed.getAuthor().getName()));
+
+			for (MessageEmbed.Field field : embed.getFields()) {
+				System.out.println(String.format("\t%s: %s", field.getName(), field.getValue()));
+			}
+
+			if (!StringUtils.isEmpty(embed.getDescription()))
+				System.out.println(String.format("\t%s", embed.getDescription()));
+			if (embed.getFooter() != null)
+				System.out.println(String.format("\tFooter: %s", embed.getFooter().getText()));
 		}
 	}
 }
