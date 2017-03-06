98d33c164167b63a4d0ae0250ed1cc5d10f57167
Michael <lolshockah@gmail.com>
2017-03-06 18:10:00 +0100

    [Groovy] WIP sandbox

--- Dunlin - Groovy/src/main/java/pl/shockah/dunlin/groovy/BasicGroovySandboxFilter.java
+++ Dunlin - Groovy/src/main/java/pl/shockah/dunlin/groovy/BasicGroovySandboxFilter.java
@@ -15,7 +15,26 @@ public class BasicGroovySandboxFilter extends GroovySandboxFilter {
                     Thread.class.getDeclaredMethod("sleep", long.class),
                     Object.class.getDeclaredMethod("toString"),
                     Object.class.getDeclaredMethod("hashCode"),
-                    Object.class.getDeclaredMethod("equals", Object.class)
+                    Object.class.getDeclaredMethod("equals", Object.class),
+                    Object.class.getDeclaredMethod("getClass"),
+                    Class.class.getDeclaredMethod("asSubclass", Class.class),
+                    Class.class.getDeclaredMethod("cast", Object.class),
+                    Class.class.getDeclaredMethod("desiredAssertionStatus"),
+                    Class.class.getDeclaredMethod("getAnnotatedInterfaces"),
+                    Class.class.getDeclaredMethod("getAnnotatedSuperclass"),
+                    Class.class.getDeclaredMethod("getAnnotation", Class.class),
+                    Class.class.getDeclaredMethod("getAnnotations"),
+                    Class.class.getDeclaredMethod("getAnnotationsByType", Class.class),
+                    Class.class.getDeclaredMethod("getCanonicalName"),
+                    Class.class.getDeclaredMethod("getClasses"),
+                    Class.class.getDeclaredMethod("getClassLoader"),
+                    Class.class.getDeclaredMethod("getComponentType"),
+                    Class.class.getDeclaredMethod("getConstructor", Class[].class),
+                    Class.class.getDeclaredMethod("getConstructors"),
+                    Class.class.getDeclaredMethod("getDeclaredAnnotations"),
+                    Class.class.getDeclaredMethod("getDeclaredAnnotation", Class.class),
+                    Class.class.getDeclaredMethod("getDeclaredAnnotationsByType", Class.class),
+                    Class.class.getDeclaredMethod("getDeclaredClasses")
             ));
         } catch (NoSuchMethodException e) {
             throw new UnexpectedException(e);
