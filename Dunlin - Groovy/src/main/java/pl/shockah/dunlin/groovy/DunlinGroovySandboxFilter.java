818d074ab6c51648d84a8efa47f6de54be3876f4
Michael <lolshockah@gmail.com>
2017-04-13 16:41:47 +0200

    [Groovy] WIP sandbox

--- Dunlin - Groovy/src/main/java/pl/shockah/dunlin/groovy/DunlinGroovySandboxFilter.java
+++ Dunlin - Groovy/src/main/java/pl/shockah/dunlin/groovy/DunlinGroovySandboxFilter.java
@@ -1,5 +1,6 @@
 package pl.shockah.dunlin.groovy;
 
+import net.dv8tion.jda.core.Permission;
 import net.dv8tion.jda.core.entities.*;
 import net.dv8tion.jda.core.requests.RestAction;
 import pl.shockah.util.UnexpectedException;
@@ -18,6 +19,17 @@ public class DunlinGroovySandboxFilter extends BasicGroovySandboxFilter {
             whitelistedMethods.addAll(Arrays.asList(
                     RestAction.class.getMethod("complete"),
                     RestAction.class.getMethod("complete", boolean.class),
+
+                    ISnowflake.class.getMethod("getId"),
+                    ISnowflake.class.getMethod("getCreationTime"),
+
+                    IMentionable.class.getMethod("getAsMention"),
+                    IPermissionHolder.class.getMethod("getGuild"),
+                    IPermissionHolder.class.getMethod("getPermissions"),
+                    IPermissionHolder.class.getMethod("hasPermission", Channel.class, Collection.class),
+                    IPermissionHolder.class.getMethod("hasPermission", Channel.class, Permission[].class),
+                    IPermissionHolder.class.getMethod("hasPermission", Collection.class),
+                    IPermissionHolder.class.getMethod("hasPermission", Permission[].class),
 
                     Guild.class.getMethod("getName"),
                     Guild.class.getMethod("getEmotesByName", String.class, boolean.class),
@@ -57,10 +69,9 @@ public class DunlinGroovySandboxFilter extends BasicGroovySandboxFilter {
                     Guild.class.getMethod("getSplashUrl"),
                     Guild.class.getMethod("getVerificationLevel"),
                     Guild.class.getMethod("getWebhooks"),
-                    //Guild.class.getMethod("getManager"),
-                    //Guild.class.getMethod("getManagerUpdatable")
-                    //Guild.class.getMethod("getController"),
-                    //Guild.class.getMethod("getAudioManager"),
+                    Guild.class.getMethod("checkVerification"),
+                    Guild.class.getMethod("isAvailable"),
+                    Guild.class.getMethod("isMember", User.class),
 
                     Member.class.getMethod("getEffectiveName"),
                     Member.class.getMethod("getColor"),
@@ -74,6 +85,10 @@ public class DunlinGroovySandboxFilter extends BasicGroovySandboxFilter {
                     Member.class.getMethod("getRoles"),
                     Member.class.getMethod("getUser"),
                     Member.class.getMethod("getVoiceState"),
+                    Member.class.getMethod("canInteract", Emote.class),
+                    Member.class.getMethod("canInteract", Member.class),
+                    Member.class.getMethod("canInteract", Role.class),
+                    Member.class.getMethod("isOwner"),
 
                     User.class.getMethod("getName"),
                     User.class.getMethod("getJDA"),
@@ -83,7 +98,62 @@ public class DunlinGroovySandboxFilter extends BasicGroovySandboxFilter {
                     User.class.getMethod("getDefaultAvatarUrl"),
                     User.class.getMethod("getEffectiveAvatarUrl"),
                     User.class.getMethod("getDiscriminator"),
-                    User.class.getMethod("getMutualGuilds")
+                    User.class.getMethod("getMutualGuilds"),
+                    User.class.getMethod("isBot"),
+
+                    TextChannel.class.getMethod("canTalk"),
+                    TextChannel.class.getMethod("canTalk", Member.class),
+                    TextChannel.class.getMethod("getTopic"),
+                    TextChannel.class.getMethod("getWebhooks"),
+
+                    Channel.class.getMethod("getName"),
+                    Channel.class.getMethod("getGuild"),
+                    Channel.class.getMethod("getInvites"),
+                    Channel.class.getMethod("getJDA"),
+                    Channel.class.getMethod("getMembers"),
+                    Channel.class.getMethod("getPermissionOverride", Member.class),
+                    Channel.class.getMethod("getPermissionOverride", Role.class),
+                    Channel.class.getMethod("getMemberPermissionOverrides"),
+                    Channel.class.getMethod("getPermissionOverrides"),
+                    Channel.class.getMethod("getPosition"),
+                    Channel.class.getMethod("getPositionRaw"),
+                    Channel.class.getMethod("getRolePermissionOverrides"),
+
+                    MessageChannel.class.getMethod("getName"),
+                    MessageChannel.class.getMethod("getJDA"),
+                    MessageChannel.class.getMethod("getPinnedMessages"),
+                    MessageChannel.class.getMethod("getType"),
+                    MessageChannel.class.getMethod("getMessageById", String.class),
+
+                    VoiceChannel.class.getMethod("getBitrate"),
+                    VoiceChannel.class.getMethod("getUserLimit"),
+
+                    VoiceState.class.getMethod("getJDA"),
+                    VoiceState.class.getMethod("getAudioChannel"),
+                    VoiceState.class.getMethod("getSessionId"),
+                    VoiceState.class.getMethod("isSelfDeafened"),
+                    VoiceState.class.getMethod("isSelfMuted"),
+
+                    Role.class.getMethod("getJDA"),
+                    Role.class.getMethod("getName"),
+                    Role.class.getMethod("canInteract", Role.class),
+                    Role.class.getMethod("getColor"),
+                    Role.class.getMethod("getGuild"),
+                    Role.class.getMethod("getPermissionsRaw"),
+                    Role.class.getMethod("getPosition"),
+                    Role.class.getMethod("getPositionRaw"),
+                    Role.class.getMethod("isHoisted"),
+                    Role.class.getMethod("isManaged"),
+                    Role.class.getMethod("isMentionable"),
+
+                    Emote.class.getMethod("isManaged"),
+                    Emote.class.getMethod("getGuild"),
+                    Emote.class.getMethod("canInteract", Member.class),
+                    Emote.class.getMethod("canInteract", User.class, MessageChannel.class),
+                    Emote.class.getMethod("getName"),
+                    Emote.class.getMethod("getAsMention"),
+                    Emote.class.getMethod("getImageUrl"),
+                    Emote.class.getMethod("getRoles")
             ));
         } catch (NoSuchMethodException e) {
             throw new UnexpectedException(e);
