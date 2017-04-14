a18b471ab4627ad13c293fe6289e3f5aba1606fc
Michael <lolshockah@gmail.com>
2017-04-14 15:16:43 +0200

    [Music] Added "Now playing: [Title by Author](url)"

--- Dunlin - Music/src/main/java/pl/shockah/dunlin/music/playlist/MessagePlaylist.java
+++ Dunlin - Music/src/main/java/pl/shockah/dunlin/music/playlist/MessagePlaylist.java
@@ -6,6 +6,7 @@ import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.VoiceChannel;
+import org.apache.commons.lang3.StringUtils;
 import pl.shockah.dunlin.music.GuildAudioManager;
 
 import java.util.Arrays;
@@ -83,19 +84,35 @@ public class MessagePlaylist extends Playlist {
 			EmbedBuilder embed = new EmbedBuilder();
 			embed.setTitle("Playlist", null);
 
-			String[] lines = tracks.stream()
-					.map(track -> String.format("**%s** by **%s**", track.getInfo().title, track.getInfo().author))
-					.toArray(String[]::new);
-			for (int i = 0; i < lines.length; i++) {
-				lines[i] = String.format("**%d**. %s", i + 1, lines[i]);
+			if (tracks.isEmpty()) {
+				embed.setDescription("Playlist is empty.");
+			} else {
+				StringBuilder sb = new StringBuilder();
+				AudioTrack playingTrack = manager.audioPlayer.getPlayingTrack();
+				if (playingTrack != null) {
+					String nowPlayingText = StringUtils.isEmpty(playingTrack.getInfo().uri) ?
+							String.format("**%s** by **%s**", playingTrack.getInfo().title, playingTrack.getInfo().author) :
+							String.format("[**%s** by **%s**](%s)", playingTrack.getInfo().title, playingTrack.getInfo().author, playingTrack.getInfo().uri);
+					sb.append(String.format("**Now playing%s:** %s", manager.audioPlayer.isPaused() ? " (paused)" : "", nowPlayingText));
+					sb.append("\n\n");
+				}
+
+				String[] lines = tracks.stream()
+						.map(track -> String.format("**%s** by **%s**", track.getInfo().title, track.getInfo().author))
+						.toArray(String[]::new);
+				for (int i = 0; i < lines.length; i++) {
+					lines[i] = String.format("**%d**. %s", i + 1, lines[i]);
+				}
+
+				lines = Arrays.stream(lines).skip(page.get() * perPage).limit(perPage).toArray(String[]::new);
+
+				sb.append(String.join("\n", lines));
+
+				embed.setDescription(sb.toString());
+				int pages = (int)Math.ceil(1.0 * tracks.size() / perPage);
+				if (pages > 1)
+					embed.setFooter(String.format("Page %d/%d", page.get() + 1, pages), null);
 			}
-
-			lines = Arrays.stream(lines).skip(page.get() * perPage).limit(perPage).toArray(String[]::new);
-
-			embed.setDescription(String.join("\n", lines));
-			int pages = (int)Math.ceil(1.0 * tracks.size() / perPage);
-			if (pages > 1)
-				embed.setFooter(String.format("Page %d/%d", page.get() + 1, pages), null);
 
 			message.editMessage(embed.build()).queue();
 		});
