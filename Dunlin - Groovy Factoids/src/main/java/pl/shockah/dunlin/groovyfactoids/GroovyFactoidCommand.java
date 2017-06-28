8e8e56de0e0d2c61a0ebc7a196c49214fc9b4e9a
Michael <lolshockah@gmail.com>
2017-06-28 11:09:27 +0200

    [Groovy Factoids] Embed output support

--- Dunlin - Groovy Factoids/src/main/java/pl/shockah/dunlin/groovyfactoids/GroovyFactoidCommand.java
+++ Dunlin - Groovy Factoids/src/main/java/pl/shockah/dunlin/groovyfactoids/GroovyFactoidCommand.java
@@ -2,7 +2,10 @@ package pl.shockah.dunlin.groovyfactoids;
 
 import groovy.lang.Binding;
 import groovy.lang.GroovyRuntimeException;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.MessageBuilder;
 import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.MessageEmbed;
 import pl.shockah.dunlin.commands.result.*;
 import pl.shockah.dunlin.factoids.AbstractFactoidCommand;
 import pl.shockah.dunlin.factoids.db.Factoid;
@@ -30,5 +33,15 @@ public class GroovyFactoidCommand extends AbstractFactoidCommand<Object, Object>
 		} catch (GroovyRuntimeException e) {
 			return new ErrorCommandResult<>(this, ErrorCommandResult.messageFromThrowable(e));
 		}
+	}
+
+	@Override
+	public Message formatOutput(Message message, Object input, Object output) {
+		if (output instanceof EmbedBuilder)
+			return new MessageBuilder().setEmbed(((EmbedBuilder)output).build()).build();
+		else if (output instanceof MessageEmbed)
+			return new MessageBuilder().setEmbed((MessageEmbed)output).build();
+		else
+			return super.formatOutput(message, input, output);
 	}
 }
