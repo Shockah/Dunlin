c6a2ba3253691f3fae61972ff739e3bac85c361a
Michael Dolas <lolshockah@gmail.com>
2017-07-16 11:10:32 +0200

    [Terraria DB] New empty plugin

---
+++ Dunlin - Terraria DB/src/main/java/pl/shockah/dunlin/terrariadb/TerrariaDBPlugin.java
@@ -0,0 +1,10 @@
+package pl.shockah.dunlin.terrariadb;
+
+import pl.shockah.dunlin.plugin.Plugin;
+import pl.shockah.dunlin.plugin.PluginManager;
+
+public class TerrariaDBPlugin extends Plugin {
+	public TerrariaDBPlugin(PluginManager manager, Info info) {
+		super(manager, info);
+	}
+}
