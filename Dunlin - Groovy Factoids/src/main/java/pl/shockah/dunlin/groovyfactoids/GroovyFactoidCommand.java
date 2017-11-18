eeb559a1e25f8ba3e6bd7d9b427ad30dd2fc096b
Michał Dolaś <me@shockah.pl>
2017-11-18 16:31:58 +0100

    [Factoids][Groovy] Groovy factoids now have access to the persisted `store` variable

--- Dunlin - Groovy Factoids/src/main/java/pl/shockah/dunlin/groovyfactoids/GroovyFactoidCommand.java
+++ Dunlin - Groovy Factoids/src/main/java/pl/shockah/dunlin/groovyfactoids/GroovyFactoidCommand.java
@@ -11,6 +11,10 @@ import pl.shockah.dunlin.commands.result.*;
 import pl.shockah.dunlin.factoids.AbstractFactoidCommand;
 import pl.shockah.dunlin.factoids.db.Factoid;
 import pl.shockah.dunlin.groovyscripting.GroovyScriptingPlugin;
+import pl.shockah.json.JSONObject;
+import pl.shockah.json.JSONPrinter;
+
+import java.util.Map;
 
 public class GroovyFactoidCommand extends AbstractFactoidCommand<Object, Object> {
 	public final GroovyScriptingPlugin scriptingPlugin;
@@ -25,16 +29,44 @@ public class GroovyFactoidCommand extends AbstractFactoidCommand<Object, Object>
 		return new ValueParseResult<>(this, textInput);
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public CommandResult<Object, Object> execute(CommandContext context, Object o) {
+		Binding binding = new Binding();
+		scriptingPlugin.injectVariables(binding, context.message);
+		binding.setVariable("input", o);
+
+		JSONObject oldStore = factoid.getStoreObject(context.message.getJDA());
+		if (oldStore != null)
+			binding.setVariable("store", oldStore);
+
 		try {
-			Binding binding = new Binding();
-			scriptingPlugin.injectVariables(binding, context.message);
-			binding.setVariable("input", o);
-			return new ValueCommandResult<>(this, scriptingPlugin.getShell(binding, context.message.getAuthor()).evaluate(factoid.getContent()));
+			Object result = scriptingPlugin.getShell(binding, context.message.getAuthor()).evaluate(factoid.getContent());
+			return new ValueCommandResult<>(this, result);
 		} catch (GroovyRuntimeException e) {
 			return new ErrorCommandResult<>(this, ErrorCommandResult.messageFromThrowable(e));
+		} finally {
+			Object newStoreObject = binding.getVariable("store");
+			if (newStoreObject != null) {
+				if (newStoreObject instanceof Map<?, ?>) {
+					JSONObject newStore = new JSONObject((Map<String, Object>)newStoreObject);
+					JSONPrinter printer = new JSONPrinter();
+
+					boolean areEqual = oldStore == newStore;
+					if (oldStore != null)
+						areEqual = printer.toString(oldStore).equals(printer.toString(newStore));
+
+					if (!areEqual)
+						saveFactoidStore(context, newStore);
+				}
+			} else if (oldStore != null) {
+				saveFactoidStore(context, null);
+			}
 		}
+	}
+
+	private void saveFactoidStore(CommandContext context, JSONObject json) {
+		factoid.setStoreObject(context.message.getJDA(), json);
 	}
 
 	@Override
